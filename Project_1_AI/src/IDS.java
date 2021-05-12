7d7dbf6a32860f581205112530b9be52c990bc91
mohammad mehdi vahedi <65023547+mahdivahedi@users.noreply.github.com>
2021-05-12 18:28:13 +0430

    Delete Project_1_AI directory

--- Project_1_AI/src/IDS.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public class IDS {
-
-    public static void main(String[] args) throws FileNotFoundException {
-        PathFinder p = new PathFinder("/home/meyti/Documents/AI/Projects/Project_1_AI/src/test1.txt");
-        p.iterativeDeepeningSearch();
-    }
-}
